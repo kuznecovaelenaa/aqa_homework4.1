06bf46cc15596cea94fa148a2d711e3dffc234c2
Elena <kuznecovaelenaa@yandex.ru>
2022-08-10 09:20:27 +0300

    change test#2

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -23,25 +25,22 @@ public class CardDeliveryTest {
         open("http://localhost:9999");
     }
 
-    public String dataCalendar() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_YEAR, 5);
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy");
-        String data = format1.format(calendar.getTime());
-        return data;
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     void shouldBeValidTest() {
+        String planningDate = generateDate(4);
         $("[data-test-id='city'] input").setValue("Москва");
         $("[data-test-id='date'] .input__control").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] .input__control").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] .input__control").sendKeys(dataCalendar());
+        $("[data-test-id='date'] .input__control").setValue(generateDate(4));
         $("[data-test-id='name'] .input__control").setValue("Кузнецова-Иванова Елена");
         $("[data-test-id='phone'] .input__control").setValue("+79150882118");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification'] .notification__content").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + dataCalendar()));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate));
     }
 }
